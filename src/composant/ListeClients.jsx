b6aed8215f17a179e28f5a7a5d87dad43d877093
hamadi mohamed <mhamadi300174@gmail.com>
2023-02-28 16:24:03 +0100

    recup entet cde

--- src/composant/ListeClients.jsx
+++ src/composant/ListeClients.jsx
@@ -7,6 +7,9 @@ const ListeClients = () => {
   const [client, setClient] = useState([]);
   const [liscli, setLiscli] = useState(true);
   const [commandeCli, setCommandeCli] = useState([]);
+  const [detailCde, setdetailCde] = useState(false);
+  const [listDetailCde, setlistDetailCde] = useState([]);
+  const [listObjet, setlistObjet] = useState([]);
   
   
   const navigate = useNavigate()
@@ -24,22 +27,43 @@ const ListeClients = () => {
     .get("http://localhost:8000/commande/client/"+codecli)
     .then((resp)=> setCommandeCli( resp.data.response),
           
-          setLiscli(false)
+          setLiscli(false),
+          
     )
   }
+  const listObjetCommande = (idObjet) =>{
+    axios
+      .get("http://localhost:8000/produit?page=1")
+      .then((resp)=>{
+        console.log("reso",resp)
+        let tabObj=resp.data.response
+        tabObj= tabObj.filter((dataObj)=>dataObj.codobj===idObjet)
+        setlistObjet(tabObj)
+        console.log(listObjet)
+      
+      })
 
-  const detailCommande = () =>{
+  }
 
+
+  const listdetailCommande = (codecde) =>{
+    axios
+    .get("http://localhost:8000/commande/detail")
+    .then((resp)=>{
+      let tab=resp.data.response
+      tab=tab.filter((data)=>data.codcde===codecde)
+      setlistDetailCde(tab)
+      listObjetCommande(tab.codobj_id)
+          })
   }
 
   const AjouterCommande = () =>{
 
   }
 
-  console.log(commandeCli)
+  //liste client
   if(liscli){return (
     <div>
-      
       <button onClick={afficheClient}>liste client</button>
       <table class="table">
         <thead>
@@ -64,7 +88,7 @@ const ListeClients = () => {
                    <td>{valeur.cpcli}</td>
                    <td>{valeur.emailcli}</td>
                    <td >
-                    <button onClick={()=>(commandeClient(valeur.codcli))}>commande associer</button>
+                    <button onClick={()=>(commandeClient(valeur.codcli))}>commande associée</button>
                    </td>
                  </tr>
             ))}
@@ -72,6 +96,7 @@ const ListeClients = () => {
       </table>
     </div>
   );}
+  //entete de commende concernat un client
   return(
     <table class="table">
     <thead>
@@ -79,26 +104,31 @@ const ListeClients = () => {
         <th scope="col">id</th>
         <th scope="col">date commande</th>
         <th scope="col">Nbr de colis</th>
-        
       </tr>
     </thead>
     <tbody>
         {commandeCli.map((valeur)=>(
                <tr>
-               <th key={valeur.codcli_id} scope="row">{valeur.id}</th>
-            
+               <th key={valeur.codcli_id} scope="row">{valeur.codcli_id}</th>
                <td>{valeur.datcde}</td>
                <td>{valeur.nbcolis}</td>
                <td >
-                <button className="btn btn-success" onClick={()=>(detailCommande())}>detaille commande</button>
+                <button className="btn btn-success" onClick={()=>(listdetailCommande(valeur.codcde))}>detaille commande</button>
                 <button  className= "btn btn-primary" onClick={()=>(AjouterCommande())}>Ajouter commande</button>
                </td>
+                {listDetailCde.map((detail)=>(
+                  <li>{detail.codobj}</li>
+                ))}
+               <li>
+              
+               </li>
              </tr>
         ))}
     </tbody>
   </table>
   
     )
+   
   
 };
 
